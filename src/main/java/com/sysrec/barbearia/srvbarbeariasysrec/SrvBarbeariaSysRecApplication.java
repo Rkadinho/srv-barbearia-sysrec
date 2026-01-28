a577870b3888d1a2381602dde9101c4744524550
rkadinho <ricardolopesroj@gmail.com>
2026-01-28 16:09:39 -0300

    chore: inicio spring boot projeto srv

---
+++ src/main/java/com/sysrec/barbearia/srvbarbeariasysrec/SrvBarbeariaSysRecApplication.java
@@ -0,0 +1,13 @@
+package com.sysrec.barbearia.srvbarbeariasysrec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvBarbeariaSysRecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrvBarbeariaSysRecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sysrec/barbearia/srvbarbeariasysrec/SrvBarbeariaSysRecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysrec.barbearia.srvbarbeariasysrec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvBarbeariaSysRecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
